569ddc4ce15a3d48b5db84ee8e1b473822533e99
WAHYU YUSOF <wahyuddin.yusof@gmail.com>
2026-04-13 20:33:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 // Tukar GAS_URL kepada URL Google Apps Script Web App anda
 // ============================================================
 const CONFIG = {
-  GAS_URL: "https://script.google.com/macros/s/AKfycbwdWWh1XUFFg1C_rzxi8BMjhqSwyIKp9ZebtXpMOnQgpFr-R0gGvn_CBs_B_xW_lb2B/exec",
+  GAS_URL: "https://script.google.com/macros/s/AKfycbzqN-NzIAPmb4WVyRdordKoaBquwkWckenpOnipuLnNUhJHEA5ACGyj2pQxd1LsOJkmhw/exec",
   ADMIN_PIN: "5542",
   INSTITUTION: "ADTEC JTM Kampus Kuantan",
   SHEET_ID: "1oIcP0CefFZf-_SKFoZt72IvQtXW7NVD5619p0INhMzw"
