0c574074f43fba4edacb96925d05627fbae7df60
WAHYU YUSOF <wahyuddin.yusof@gmail.com>
2026-04-13 20:29:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ const CONFIG = {
   GAS_URL: "https://script.google.com/macros/s/AKfycbzRfUX9aKtt_cprjN7J5gQw_ClzennY6TFx-3SZc7xbMew_rPw7FHXeoY-qWewdq09q/exec",
   ADMIN_PIN: "5542",
   INSTITUTION: "ADTEC JTM Kampus Kuantan",
-  SHEET_ID: "1JYsN2Ozl4LQqIYMsq9FO_QETgspB5qSrTqD1oq4u2iI"
+  SHEET_ID: "1oIcP0CefFZf-_SKFoZt72IvQtXW7NVD5619p0INhMzw"
 };
